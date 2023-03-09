491b14d0a8ee19e929decd3c94651d244bc889f1
kmachnickiwork49 <kmachnickiwork@gmail.com>
2023-03-08 23:29:25 -0800

    Added title screen

--- Assets/Scripts/TitleButtonsInteraction.cs
+++ Assets/Scripts/TitleButtonsInteraction.cs
@@ -1,20 +1,23 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleButtonsInteraction : MonoBehaviour
 {
 
+    [SerializeField] public string myScene;
     void Start()
     {
-        OnMouseDown();
-        OnMouseOver();
-        OnMouseExit();
+        //OnMouseDown();
+        //OnMouseOver();
+        //OnMouseExit();
     }
 
     void OnMouseDown()
     {
         Debug.Log("BUTTON PRESSED");
+        SceneManager.LoadScene(myScene);
         //if play button, load game scene
         //if credits, open credit menu
         //if quit, quit the game
