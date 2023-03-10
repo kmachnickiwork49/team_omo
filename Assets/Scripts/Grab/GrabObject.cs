5e825667b619e5b28fb9778b0ec28e6e9d78ed0c
kmachnickiwork49 <kmachnickiwork@gmail.com>
2023-03-10 00:08:48 -0800

    Updated grab to be more point-and-click

--- Assets/Scripts/Grab/GrabObject.cs
+++ Assets/Scripts/Grab/GrabObject.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class GrabObject : MonoBehaviour
 {
+    [SerializeField] private Camera m_camera;
+
     [SerializeField]
     private Transform grabPoint;
+
+    private Vector2 grabVec;
 
     [SerializeField]
     private Transform rayPoint;
     [SerializeField]
     private float rayDistance;
+
+    private float offsetDist;
 
     private GameObject grabbedObject;
     private int layerIndex; 
@@ -23,24 +29,36 @@ public class GrabObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
+        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, (mousePos-(Vector2)rayPoint.position), rayDistance, LayerMask.GetMask("GrabbableObject"));
+        //Debug.Log(hitInfo.collider);
+
+        if (grabbedObject != null) {
+            grabbedObject.transform.position = new Vector3(grabVec.x + gameObject.transform.position.x, grabVec.y + gameObject.transform.position.y, grabbedObject.transform.position.z);
+        } 
+
+        if (Input.GetKeyDown(KeyCode.E) && grabbedObject != null)
+        {
+            grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            grabbedObject.transform.SetParent(null);
+            grabbedObject = null;
+            return; // Do not drop and grab same frame
+        }
+
         if (hitInfo.collider != null && hitInfo.collider.gameObject.layer == layerIndex)
         {
             if (Input.GetKeyDown(KeyCode.E) && grabbedObject == null) 
             {
                 grabbedObject = hitInfo.collider.gameObject;
                 grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
-                grabbedObject.transform.position = grabPoint.position;
-                grabbedObject.transform.SetParent(transform);
+                //grabbedObject.transform.position = grabPoint.position;
+                //grabbedObject.transform.position = hitInfo.point;
+                offsetDist = Mathf.Max(1.01f, hitInfo.distance); // OMO radius
+                grabVec = (Vector2)grabbedObject.transform.position - (Vector2)hitInfo.point + (Vector2)rayPoint.position + (mousePos-(Vector2)rayPoint.position).normalized * offsetDist - (Vector2)gameObject.transform.position;
+                //grabbedObject.transform.SetParent(transform);
             }
-            else if (Input.GetKeyDown(KeyCode.E))
-            {
-                grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
-                grabbedObject.transform.SetParent(null);
-                grabbedObject = null;
-            }
-        Debug.Log("Touch");
+            //Debug.Log("Touch");
         }
-        Debug.DrawRay(rayPoint.position, transform.right * rayDistance);       
+        Debug.DrawRay(rayPoint.position, (mousePos-(Vector2)rayPoint.position).normalized * rayDistance);       
     }
 }
