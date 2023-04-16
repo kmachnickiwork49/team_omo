a67f8fc8910a17e9ff3b4000bba0e2add2ea512b
Stefan Abgarian <stefan.abgarian@gmail.com>
2023-04-16 12:34:41 -0700

    Added health bar and scene reset system

---
+++ Assets/Scripts/PlayerScripts/HealthController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthController : MonoBehaviour
+{
+
+    public int maxHealth = 4;
+    public int currentHealth;
+    public string currentSceneName;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            takeDamage(1);
+            if (currentHealth == 0) 
+            {
+                SceneManager.LoadScene(currentSceneName);
+            }
+        }
+    }
+
+    void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
