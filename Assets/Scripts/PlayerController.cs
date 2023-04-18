9e4da713909b3426537e86f5b426f826ef3d5406
kmachnickiwork49 <kmachnickiwork@gmail.com>
2023-04-17 21:39:12 -0700

    Looks good for single grav source, will try to get multiple grav source directional indicator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -84,11 +84,7 @@ public class PlayerController : MonoBehaviour
 
     public void Death() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    public void Death() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+    }   
 
 
     void OnCollisionEnter2D(Collision2D collision) {
