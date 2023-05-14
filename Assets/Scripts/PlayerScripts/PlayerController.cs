81c4d996c977b14c4706663a27e573f5b3b8246f
Stefan Abgarian <stefan.abgarian@gmail.com>
2023-05-13 22:57:56 -0700

    Added rudementary teleportation system

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -86,10 +86,6 @@ public class PlayerController : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void Death() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Ground") {
